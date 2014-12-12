cb617cb87bce39995a05b54a46af67a9438a3fb4
Satoshi Konno <skonno@cybergarage.org>
2014-12-12 14:36:25 +0900

    * Added RoundMessageManagerThreadTest.

--- tests/common/MessageTests.cpp
+++ tests/common/MessageTests.cpp
@@ -14,17 +14,18 @@
 #include <time.h>
 
 #include <round/common/Message.h>
+#include <round/common/Thread.h>
 
 using namespace std;
 using namespace Round;
 
-#define FRACTAL_MESSAGE_BASIC_TEST_COUNT 10
+#define ROUND_MSGMRG_TEST_COUNT 10
 
 BOOST_AUTO_TEST_CASE(RoundMessageManagerTest) {
   MessageManager msgMgr;
 
   char key[32], value[32];
-  for (int n = 0; n < FRACTAL_MESSAGE_BASIC_TEST_COUNT; n++) {
+  for (int n = 0; n < ROUND_MSGMRG_TEST_COUNT; n++) {
     snprintf(key, sizeof(key), "key%d", n);
     snprintf(value, sizeof(value), "%d", n);
     Message *msg = new Message();
@@ -32,7 +33,7 @@ BOOST_AUTO_TEST_CASE(RoundMessageManagerTest) {
     BOOST_CHECK(msgMgr.pushMessage(msg));
   }
 
-  for (int n = 0; n < FRACTAL_MESSAGE_BASIC_TEST_COUNT; n++) {
+  for (int n = 0; n < ROUND_MSGMRG_TEST_COUNT; n++) {
     snprintf(key, sizeof(key), "key%d", n);
     snprintf(value, sizeof(value), "%d", n);
     const Message *msg;
@@ -42,4 +43,46 @@ BOOST_AUTO_TEST_CASE(RoundMessageManagerTest) {
     BOOST_CHECK_EQUAL(value.compare(value), 0);
     delete msg;
   }
+}
+
+class MsgPushThread : public Thread<MessageManager> {
+public:
+  
+  MsgPushThread() {
+  }
+  
+  void run() {
+    MessageManager *msgMgr = getObject();
+    char key[32], value[32];
+    for (int n = 0; n < ROUND_MSGMRG_TEST_COUNT; n++) {
+      snprintf(key, sizeof(key), "key%d", n);
+      snprintf(value, sizeof(value), "%d", n);
+      Message *msg = new Message();
+      msg->set(key, value);
+      sleep(1000);
+      BOOST_CHECK(msgMgr->pushMessage(msg));
+    }
+  }
+};
+
+BOOST_AUTO_TEST_CASE(RoundMessageManagerThreadTest) {
+  MessageManager msgMgr;
+  
+  MsgPushThread msgPushThread;
+  msgPushThread.setObject(&msgMgr);
+  BOOST_CHECK(msgPushThread.start());
+  
+  char key[32], value[32];
+  for (int n = 0; n < ROUND_MSGMRG_TEST_COUNT; n++) {
+    snprintf(key, sizeof(key), "key%d", n);
+    snprintf(value, sizeof(value), "%d", n);
+    const Message *msg;
+    BOOST_CHECK(msgMgr.waitMessage(&msg));
+    string value;
+    BOOST_CHECK(msg->get(key, &value));
+    BOOST_CHECK_EQUAL(value.compare(value), 0);
+    delete msg;
+  }
+
+  BOOST_CHECK(msgPushThread.stop());
 }
