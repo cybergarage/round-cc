3c19254ad62d363b86e396c24276bce5452d1891
Satoshi Konno <skonno@cybergarage.org>
2015-01-10 10:45:57 +0900

    * Added tcl.rb for Homebrew

---
+++ pkg/homebrew/tcl.rb
@@ -0,0 +1,16 @@
+require "formula"
+
+class Tcl < Formula
+  homepage ""
+  url "http://downloads.sourceforge.net/project/tcl/Tcl/8.6.3/tcl8.6.3-src.tar.gz"
+  sha1 "026b4b6330205bdc49af12332ee17c2b01f76d37"
+
+  def install
+    system "cd", "macosx"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+end
