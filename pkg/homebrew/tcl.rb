0a3d5a471860eb6824627187a882fca5b81af206
Satoshi Konno <skonno@cybergarage.org>
2015-01-10 11:57:34 +0900

    * Added homepage for Tcl.

--- pkg/homebrew/tcl.rb
+++ pkg/homebrew/tcl.rb
@@ -1,7 +1,7 @@
 require "formula"
 
 class Tcl < Formula
-  homepage ""
+  homepage "http://www.tcl.tk/"
   url "http://downloads.sourceforge.net/project/tcl/Tcl/8.6.3/tcl8.6.3-src.tar.gz"
   sha1 "026b4b6330205bdc49af12332ee17c2b01f76d37"
 
