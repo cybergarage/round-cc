c48c1688af80bac28c90d4b248559c26193af3c7
Satoshi Konno <skonno@cybergarage.org>
2015-01-19 20:59:19 +0900

    * Updated list().

--- src/round/ui/console/cmd/list.cpp
+++ src/round/ui/console/cmd/list.cpp
@@ -9,6 +9,7 @@
  ******************************************************************/
 
 #include <iostream>
+#include <round/core/Node.h>
 #include <round/ui/console/Command.h>
 
 const std::string Round::Console::list::NAME = "list";
@@ -19,10 +20,28 @@ bool Round::Console::list::exec(Round::Console::Client *client, const Params *pa
   size_t clusterCnt = 0;
   for (ClusterList::const_iterator cluster = clusters->begin(); cluster != clusters->end(); cluster++) {
     
-    std::string clusterName = (*cluster)->getName();
-    std::cout << "[" << clusterCnt << "] " << clusterName << std::endl;
+    std::cout << "[" << clusterCnt << "] " << (*cluster)->getName() << std::endl;
     
-    cluster++;
+    const NodeGraph *nodeGraph = (*cluster)->getNodeGraph();
+    size_t nodeCnt = 0;
+    for (NodeGraph::const_iterator nodeIt = nodeGraph->begin(); nodeIt != nodeGraph->end(); nodeIt++) {
+      ConsistentHashNode *cnode = *nodeIt;
+      Node *node = dynamic_cast<Node *>(cnode);
+      if (!node)
+        continue;
+      std::string hash;
+      node->getHashCode(&hash);
+      std::string host;
+      if (node->getRequestAddress(&host, err))
+        return false;
+      int port;
+      if (node->getRequestPort(&port, err))
+        return false;
+      std::cout << "[" << nodeCnt << "] " << host << ":" << port << " (" << hash << ")" << std::endl;
+      nodeCnt++;
+    }
+    
+    clusterCnt++;
   }
   return true;
 }
