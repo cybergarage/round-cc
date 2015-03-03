97129dfe4664f664069536743b16d00dea92b5b1
Satoshi Konno <skonno@cybergarage.org>
2015-03-03 21:07:07 +0900

    * Added Const.h.get to create Const.h from github.

---
+++ include/round/core/Const.h
@@ -0,0 +1,16 @@
+/******************************************************************
+ *
+ * Round for C++
+ *
+ * Copyright (C) Satoshi Konno 2015
+ *
+ * This is licensed under BSD-style license, see file COPYING.
+ *
+ ******************************************************************/
+
+#ifndef _ROUNDCC_CONST_H_
+#define _ROUNDCC_CONST_H_
+
+#define ROUNDCC_HTTP_RPC_URI /rpc
+
+#endif
