4a9d81e62185f11386ac1325f6bc4185ce81290e
Satoshi Konno <skonno@cybergarage.org>
2015-03-24 21:07:05 +0900

    * Updated LuaEngine::run().

--- src/round/script/LuaEngine.cpp
+++ src/round/script/LuaEngine.cpp
@@ -52,14 +52,29 @@ bool Round::LuaEngine::compile(const Script *luaScript) const {
 ////////////////////////////////////////////////
 
 bool Round::LuaEngine::run(const Script *luaScript, const std::string &params, std::string *results, Error *error) const {
+  int nStack = lua_gettop(this->luaState);
+  
   if (luaL_loadstring(this->luaState, (const char *)luaScript->getCode()) != 0) {
     RPC::JSON::ErrorCodeToError(RPC::JSON::ErrorCodeInternalError, error);
     return false;
   }
+
+  nStack = lua_gettop(this->luaState);
+  
+  if(lua_pcall(this->luaState, 0, 0, 0) != 0) {
+    RPC::JSON::ErrorCodeToError(RPC::JSON::ErrorCodeInternalError, error);
+    error->setMessage(lua_tostring(this->luaState, -1));
+    lua_pop(this->luaState, 1);
+    return false;
+  }
+  
+  nStack = lua_gettop(this->luaState);
   
   lua_getglobal(this->luaState, luaScript->getName().c_str());
   lua_pushstring(this->luaState, params.c_str());
 
+  nStack = lua_gettop(this->luaState);
+  
   int callResult = lua_pcall(this->luaState, 1, 1, 0);
   if (callResult == 0) {
     *results = lua_tostring(this->luaState, -1);
@@ -69,8 +84,12 @@ bool Round::LuaEngine::run(const Script *luaScript, const std::string &params, s
     error->setMessage(lua_tostring(this->luaState, -1));
   }
   
-  lua_pop(this->luaState, 2);
+  nStack = lua_gettop(this->luaState);
   
+  lua_pop(this->luaState, 1);
+  
+  nStack = lua_gettop(this->luaState);
+ 
   return (callResult == 0) ? true : false;
 }
 
