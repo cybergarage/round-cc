e4d4f93a70f489f759747fe11d5bbc61660862e5
Satoshi Konno <skonno@cybergarage.org>
2015-05-04 22:00:15 +0900

    * Added LuaEngine::popResult() and popError().

--- include/round/script/Lua.h
+++ include/round/script/Lua.h
@@ -11,6 +11,7 @@
 #ifndef _ROUNDCC_SYSTEM_METHOD_LUA_H_
 #define _ROUNDCC_SYSTEM_METHOD_LUA_H_
 
+#include <string>
 #include <round/core/Script.h>
 
 extern "C" {
@@ -34,6 +35,9 @@ class LuaEngine : public ScriptEngine {
   bool compile(const Script *script) const;
   bool run(const Script *script, const std::string &params, std::string *results, Error *error) const;
   bool run(const std::string &script, std::string *result, Error *error) const;
+
+  bool popString(std::string *result) const;
+  bool popError(Error *error) const;
 
 private:
   
--- src/round/script/LuaEngine.cpp
+++ src/round/script/LuaEngine.cpp
@@ -9,6 +9,7 @@
 ******************************************************************/
 
 #include <sstream>
+#include <boost/lexical_cast.hpp>
 #include <boost/algorithm/string/replace.hpp>
 
 #include <round/Const.h>
@@ -34,8 +35,11 @@ Round::LuaEngine::LuaEngine() : ScriptEngine(LANGUAGE) {
   lua_register(this->luaState, ROUNDCC_SYSTEM_METHOD_GET_NETWORK_STATE, round_lua_getnetworkstate);
   lua_register(this->luaState, ROUNDCC_SYSTEM_METHOD_GET_CLUSTER_STATE, round_lua_getclusterstate);
   lua_register(this->luaState, ROUNDCC_SYSTEM_METHOD_GET_NODE_STATE, round_lua_getnodestate);
+  
   lua_register(this->luaState, ROUNDCC_SYSTEM_METHOD_SET_REGISTRY, round_lua_setregistry);
   lua_register(this->luaState, ROUNDCC_SYSTEM_METHOD_GET_REGISTRY, round_lua_getregistry);
+  lua_register(this->luaState, ROUNDCC_SYSTEM_METHOD_REMOVE_REGISTRY, round_lua_removeregistry);
+
   lua_register(this->luaState, ROUNDCC_SCRIPT_POST_METHOD, round_lua_postmethod);
 }
 
@@ -62,6 +66,45 @@ bool Round::LuaEngine::compile(const Script *luaScript) const {
 // run
 ////////////////////////////////////////////////
 
+bool Round::LuaEngine::popString(std::string *result) const {
+  int nStack = lua_gettop(this->luaState);
+  if (nStack <= 0)
+    return false;
+
+  if (lua_isstring(this->luaState, -1)) {
+    *result = lua_tostring(this->luaState, -1);
+    lua_pop(this->luaState, 1);
+    return true;
+  }
+
+  if (lua_isboolean(this->luaState, -1)) {
+    *result = lua_toboolean(this->luaState, -1) ? "true" : "false";
+    lua_pop(this->luaState, 1);
+    return true;
+  }
+  
+  if (lua_isnumber(this->luaState, -1)) {
+    double dresult = lua_tonumber(this->luaState, -1);
+    *result = boost::lexical_cast<std::string>(dresult);
+    lua_pop(this->luaState, 1);
+    return true;
+  }
+
+  return false;
+}
+
+bool Round::LuaEngine::popError(Error *error) const {
+  int nStack = lua_gettop(this->luaState);
+  if (nStack <= 0)
+    return false;
+
+  RPC::JSON::ErrorCodeToError(RPC::JSON::ErrorCodeInternalError, error);
+  error->setMessage(lua_tostring(this->luaState, -1));
+  lua_pop(this->luaState, 1);
+  
+  return true;
+}
+
 bool Round::LuaEngine::run(const Script *luaScript, const std::string &params, std::string *results, Error *error) const {
   lock();
   
@@ -70,20 +113,9 @@ bool Round::LuaEngine::run(const Script *luaScript, const std::string &params, s
   int nStack = lua_gettop(this->luaState);
   
   if (luaL_loadstring(this->luaState, (const char *)luaScript->getCode()) != 0) {
-    nStack = lua_gettop(this->luaState);
-    
-    if (0 < nStack) {
-      RPC::JSON::ErrorCodeToError(RPC::JSON::ErrorCodeInternalError, error);
-      error->setMessage(lua_tostring(this->luaState, -1));
-      lua_pop(this->luaState, 1);
-    }
-    
-    nStack = lua_gettop(this->luaState);
-
+    popError(error);
     round_lua_setlocalnode(NULL);
-    
     unlock();
-    
     return false;
   }
 
@@ -117,17 +149,10 @@ bool Round::LuaEngine::run(const Script *luaScript, const std::string &params, s
   int callResult = lua_pcall(this->luaState, 1, 1, 0);
   nStack = lua_gettop(this->luaState);
   if (callResult == 0) {
-    if (0 < nStack) {
-      *results = lua_tostring(this->luaState, -1);
-      lua_pop(this->luaState, 1);
-    }
+    popString(results);
   }
   else {
-    if (0 < nStack) {
-      RPC::JSON::ErrorCodeToError(RPC::JSON::ErrorCodeInternalError, error);
-      error->setMessage(lua_tostring(this->luaState, -1));
-      lua_pop(this->luaState, 1);
-    }
+    popError(error);
   }
   
   nStack = lua_gettop(this->luaState);
@@ -149,17 +174,10 @@ bool Round::LuaEngine::run(const std::string &script, std::string *results, Erro
   int callResult = luaL_dostring (this->luaState, script.c_str());
   nStack = lua_gettop(this->luaState);
   if (callResult == 0) {
-    if (0 < nStack) {
-      *results = lua_tostring(this->luaState, -1);
-      lua_pop(this->luaState, 1);
-    }
+    popString(results);
   }
   else {
-    if (0 < nStack) {
-      RPC::JSON::ErrorCodeToError(RPC::JSON::ErrorCodeInternalError, error);
-      error->setMessage(lua_tostring(this->luaState, -1));
-      lua_pop(this->luaState, 1);
-    }
+    popError(error);
   }
   
   nStack = lua_gettop(this->luaState);
